ac0e31d222535b78591090cc19a8c85d59c6e956
tylercchase <tylercchase@gmail.com>
2022-04-23 23:24:46 -0800

    header image

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import GameBlock from './Game';
 import { Game } from './models/Game';
+import thing from './wordart.png';
 function App() {
 
   const [games, setGames] = useState<Game[]>([
-      {
-        name: 'Game 1',
-        urls: {
-          status: 'active',
-          stop: '',
-          start: ''
-        }
-      },
-      {
-        name: 'Game 2',
-        urls: {
-          status: 'stopped',
-          stop: '',
-          start: ''
-        }
-      },
+    {
+      name: 'Game 1',
+      urls: {
+        status: 'active',
+        stop: '',
+        start: ''
+      }
+    },
+    {
+      name: 'Game 2',
+      urls: {
+        status: 'stopped',
+        stop: '',
+        start: ''
+      }
+    },
   ]);
   useEffect(() => {
     fetch('https://us-west1-game-management-348123.cloudfunctions.net/info').then(res => res.json()).then(data => {
@@ -29,10 +30,12 @@ function App() {
   }, []);
   return (
     <div>
-      <h1></h1>
-    <div className="games">
-      {games.map(game => <GameBlock game={game} key={game.name}/>)}
-    </div>
+      <div className="header">
+        <img src={thing} alt="thing" />
+      </div>
+      <div className="games">
+        {games.map(game => <GameBlock game={game} key={game.name} />)}
+      </div>
     </div>
   );
 }
