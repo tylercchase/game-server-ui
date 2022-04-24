7f246517fdc3777cb2af3f579d477e99d6034239
tylercchase <tylercchase@gmail.com>
2022-04-23 23:40:29 -0800

    update prop dependency

--- src/Game.tsx
+++ src/Game.tsx
@@ -2,20 +2,21 @@ import { useEffect, useState } from 'react';
 import './Game.css';
 import { Game } from './models/Game';
 function GameBlock(props: { game: Game }) {
-    const [status, setStatus] = useState(props.game.urls.status);
+    const [status, setStatus] = useState("STAGING");
     useEffect(() => {
         fetch(props.game.urls.status).then(res => res.json()).then(data => {
             setStatus(data['status']);
         });
-    }, []);
+    }, [props.game.urls.status]);
     function handleClick(url: string) {
+        setStatus('STAGING');
         fetch(url).then(res => res.json()).then(res => setStatus(res['status']));
     }
     return (
         <div className="game">
             <h3>{props.game.name}</h3>
             <div className="game-status">
-                <span className={`${status}`}>{status}</span>
+                <span className={`status ${status}`}>{status}</span>
             </div>
             <div className="game-buttons">
                 {status === 'RUNNING' &&
